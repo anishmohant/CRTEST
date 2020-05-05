438c049be276f8bb20f2360ce7dfa2233b041fbe
anishmohant <anishmohant@gmail.com>
2020-05-05 10:45:57 +0530

    added fix to new registration without ref code

--- userlist/views.py
+++ userlist/views.py
@@ -55,30 +55,35 @@ def enlist(request):
         referer_code = request.POST.get('code', None)
         print("dsd")
         print(referer_code)
+        print(type(referer_code))
         total_entries = UserList.objects.all().count()
         own_code = str(uuid.uuid1())
         if total_entries > 0:
-            referer = UserList.objects.filter(own_code=referer_code)
-            referer_position = referer[0].position
-            print(referer)
-            if referer_position == 2:
-                print("This guy is going places")
+            if referer_code =='':
+                max = UserList.objects.aggregate(Max('position'))
+                last_position = int(max['position__max'])
+                user_entry = UserList(name=name, email=email, refer_code=referer_code, own_code=own_code,
+                                      position=last_position + 1)
+                user_entry.save()
+                initiate_email(email, int(request.META['SERVER_PORT']), own_code)
+            else:
                 referer = UserList.objects.filter(own_code=referer_code)
-                email = referer[0].email
-                print(email)
-                initiate_email(email)
-            UserList.objects.filter(own_code=referer_code).update(position=referer_position - 1)
-            max = UserList.objects.aggregate(Max('position'))
-            last_position = int(max['position__max'])
-            user_entry = UserList(name=name, email=email, refer_code=referer_code, own_code=own_code, position=last_position+1)
-            user_entry.save()
-            initiate_email(email, int(request.META['SERVER_PORT']), own_code)
+                referer_position = referer[0].position
+                print(referer)
+                if referer_position == 2:
+                    referer = UserList.objects.filter(own_code=referer_code)
+                    email = referer[0].email
+                    initiate_email(email)
+                UserList.objects.filter(own_code=referer_code).update(position=referer_position - 1)
+                max = UserList.objects.aggregate(Max('position'))
+                last_position = int(max['position__max'])
+                user_entry = UserList(name=name, email=email, refer_code=referer_code, own_code=own_code, position=last_position+1)
+                user_entry.save()
+                initiate_email(email, int(request.META['SERVER_PORT']), own_code)
 
         else:
             user_entry = UserList(name=name, email=email, refer_code=referer_code, own_code=own_code, position=5)
             user_entry.save()
-            initiate_email( email,int(request.META['SERVER_PORT']), own_code)
+            initiate_email(email, int(request.META['SERVER_PORT']), own_code)
 
         return render(request, 'index.html', {})
-# UserList.objects.get(own_code='0b9545ac-8dee-11ea-8f01-8cdcd449aef0')
-# UserList.objects.get(name='baba')
